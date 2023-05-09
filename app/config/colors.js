080ec2d84e684800a43cf09fe4896aee66063ebb
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-05-09 21:38:31 +0300

    add color config

---
+++ app/config/colors.js
@@ -0,0 +1,10 @@
+export default {
+  primary: "#F28500",
+  secondary: "#cc5500",
+  black: "#000",
+  white: "#fff",
+  medium: "#fbec5d",
+  light: "#f5f5f5",
+  dark: "#0c0c0c",
+  danger: "#ff5252",
+};
--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
 import Records from "../screens/Records";
 import Home from "../screens/Home";
 import Account from "../screens/Account";
@@ -10,9 +12,37 @@ const Tab = createBottomTabNavigator();
 function AppNavigator(props) {
   return (
     <Tab.Navigator initialRouteName="Home">
-      <Tab.Screen name="Records" component={Records} />
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Account" component={Account} />
+      <Tab.Screen
+        name="Records"
+        component={Records}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="equalizer"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={Account}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
